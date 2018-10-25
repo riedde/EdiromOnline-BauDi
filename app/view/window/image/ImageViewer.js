4e8e51d881198de9b37a2d9c5879467cd1f111b8
Dennis Ried <ried-musikforschung@mail.de>
2018-10-26 00:24:56 +0200

    Pfad für Digilib

--- app/view/window/image/ImageViewer.js
+++ app/view/window/image/ImageViewer.js
@@ -42,7 +42,7 @@ Ext.define('EdiromOnline.view.window.image.ImageViewer', {
     imgWidth: 0,
     imgHeight: 0,
 
-    imgPrefix: null,
+    imgPrefix: 'http://localhost:8081/digilib/Scaler/',
     
     shapes: null,
     shapesHidden: false,
