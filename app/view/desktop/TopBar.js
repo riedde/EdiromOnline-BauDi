7ee1821869133a163907b912e222a5610ecb7e1e
Dennis Ried <ried-musikforschung@mail.de>
2018-10-26 08:48:15 +0200

    Edition-Name in Toolbar changed

--- app/view/desktop/TopBar.js
+++ app/view/desktop/TopBar.js
@@ -70,7 +70,7 @@ Ext.define('EdiromOnline.view.desktop.TopBar', {
                 cls: 'ux-desktop-topbar-flex',
                 items: [
                 		me.homeButton,
-						{ xtype: 'tbtext', text: 'Reger-Werkausgabe', id: 'homeBtnLabel' },
+						{ xtype: 'tbtext', text: 'Baumann Digital', id: 'homeBtnLabel' },
 						this.workCombo,
 						{
 							xtype: 'splitter',
